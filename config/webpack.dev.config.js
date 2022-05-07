64219943b4d595b0b1a22a2f41bdcab93eea6841
Eric Yao <luna8418@gmail.com>
2022-05-07 22:03:21 +1000

    Task-1: Change default port to be 3000

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 const HOST = process.env.HOST || '127.0.0.1';
-const PORT = process.env.PORT || '9000';
+const PORT = process.env.PORT || '3000';
 
 const config = {
   mode: 'development',
