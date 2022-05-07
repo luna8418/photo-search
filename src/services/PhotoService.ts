d13661c8b9b94e1820fa0f88f57c56ff0c428750
Eric Yao <luna8418@gmail.com>
2022-05-07 23:55:28 +1000

    Task-1: Add PhotoService

---
+++ src/services/PhotoService.ts
@@ -0,0 +1,41 @@
+import { GraphQLClient, gql } from 'graphql-request';
+import { Photo } from '../types/types';
+
+export class PhotoService {
+  private client: GraphQLClient;
+
+  constructor() {
+    this.client = new GraphQLClient('https://graphqlzero.almansi.me/api');
+  }
+
+  searchPhotos = async (keyword: string, page: number, size: number): Promise<{ photos: Photo[], total: number }> => {
+    const query = gql`{
+      photos(options: {
+        search: {
+          q: "${keyword}"
+        }
+        paginate: {
+          page: ${page}
+          limit: ${size}
+        }
+      }) {
+        data {
+          id
+          title
+          thumbnailUrl
+          url
+        }
+        meta {
+          totalCount
+        }
+      }
+    }`
+
+    const data = await this.client.request(query);
+
+    return {
+      photos: data.photos.data,
+      total: data.photos.meta.totalCount
+    }
+  }
+}
